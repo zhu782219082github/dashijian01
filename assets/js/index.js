15b5b3ed45d17d51e7a94018bd5569eedcc271ba
zhuzhu <782219082@qq.com>
2020-12-07 11:31:30 +0800

    '修改了base'

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,46 @@
 $(function() {
+    var layer = layui.layer;
     // 调用 getUserInfo 获取用户基本信息
-    getUserInfo()
-
-
-var  layer=layui.layer;
+    getUserInfo();
   $('#btnLogout').on('click',function(){
     //   提示用户是否确认退出
     layer.confirm('确定要退出登录吗?', {icon: 3, title:'提示'}, function(index){
         //do something
-        
+        localStorage.removeItem('token')
+        location.href='/login.html'
         layer.close(index);
       });
   })
+
+
+
 // 获取用户的基本信息
 function getUserInfo() {
     $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
+        method: 'GET',
+        url: '/my/userinfo',
       // headers 就是请求头配置对象
-      Headers: {
-        Authorization: localStorage.getItem('token') || ''
-      },
+    //   headers: {
+    //     Authorization: localStorage.getItem('token') || ''
+    //   },
       success: function(res) {
         if (res.status !== 0) {
           return layui.layer.msg('获取用户信息失败！')
         }
+        console.log(res);
         // 调用 renderAvatar 渲染用户的头像
         renderAvatar(res.data)
-      }
+      },
+
+// 不论成功还是失败，最终都会调用 complete 回调函数
+complete:function(res){
+    // console.log('执行了');
+    if (res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败'){
+        localStorage.removeItem('token')
+        location.href='/login.html'
+    }
+}
+
     })
   }
 // 渲染用户的头像
@@ -52,4 +65,11 @@ function renderAvatar(user) {
         .show()
     }
   }
+
+
+
+
+
+
+//结束的括号
 })
